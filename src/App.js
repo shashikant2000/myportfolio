0057426281b2c0d2a591ddba9074637a79a2e177
Shashikant Kumar <shashikantkumar@Shashikants-MacBook-Air.local>
2024-09-09 19:09:22 +0530

    used hashrouter instead of browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { AnimatePresence } from 'framer-motion';
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import AppFooter from './components/shared/AppFooter';
 import AppHeader from './components/shared/AppHeader';
@@ -18,7 +18,7 @@ function App() {
 	return (
 		<AnimatePresence>
 			<div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
-				<Router basename="/myportfolio">
+				<Router>
 					<ScrollToTop />
 					<AppHeader />
 					<Suspense fallback={""}>

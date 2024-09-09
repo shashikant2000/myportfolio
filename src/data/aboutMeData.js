9c3222b438ce5b609df9827af89578cca913b0df
Shashikant Kumar <shashikantkumar@Shashikants-MacBook-Air.local>
2024-09-10 01:38:02 +0530

    changed bio

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,10 @@
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: "With a Bachelor's degree in Computer Engineering from Indraprastha Institute of Information Technology, Delhi, my journey has led me to Qualcomm, where I currently excel as a Full-Stack Developer. My core competencies lie in front-end development, with a focus on creating dynamic desktop applications and enhancing web components.",
+		bio: "I’m Shashikant, a software developer at Qualcomm, working on impactful software projects. I’ve developed a widely adopted cross-framework component library for Angular and React and created a desktop application with Electron for visualizing chipset performance metrics. My expertise includes using Tailwind CSS, TypeScript, and other technologies to build robust and scalable solutions.",
 	},
 	{
 		id: 2,
-		bio: "At Qualcomm, my team and I have integrated a cross-framework component library across 15+ applications, leading to streamlined development and consistent user experiences. We thrive on collaboration and are committed to aligning our innovative solutions with the organization's mission, fostering a culture of continuous improvement and shared success.",
+		bio: "In addition to these projects, I focus on interactive testing automation tools for Qualcomm’s latest chips and platforms. My background includes a BTech from IIIT Delhi, which has provided me with a solid technical foundation and problem-solving skills essential for tackling complex software development challenges.",
 	},
 ];
